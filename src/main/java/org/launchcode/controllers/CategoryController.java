f4a4c6cb0ff1d5ef9cfe780ec8b3dcd8d99036a0
Cobe Bailey <baileycobe@yahoo.com>
2019-04-25 17:43:16 -0500

    finally diddly done2

--- src/main/java/org/launchcode/controllers/CategoryController.java
+++ src/main/java/org/launchcode/controllers/CategoryController.java
@@ -1,10 +1,8 @@
 package org.launchcode.controllers;
 
+
 import org.launchcode.models.Category;
-import org.launchcode.models.Cheese;
-import org.launchcode.models.CheeseType;
 import org.launchcode.models.data.CategoryDao;
-import org.launchcode.models.data.CheeseDao;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -12,81 +10,50 @@ import org.springframework.validation.Errors;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
 
 import javax.validation.Valid;
-import java.util.List;
 
-/**
- * Created by LaunchCode
- */
 @Controller
-@RequestMapping("cheese")
-public class CheeseController {
+@RequestMapping("category")
+public class CategoryController {
 
     @Autowired
-    CheeseDao cheeseDao;
+    private CategoryDao categoryDao;
 
-    @Autowired
-    CategoryDao categoryDao;
-
-    // Request path: /cheese
     @RequestMapping(value = "")
     public String index(Model model) {
 
-        model.addAttribute("cheeses", cheeseDao.findAll());
-        model.addAttribute("title", "My Cheeses");
+        model.addAttribute("categories", categoryDao.findAll());
+        model.addAttribute("title", "Categories");
 
-        return "cheese/index";
+        return "category/index";
     }
 
     @RequestMapping(value = "add", method = RequestMethod.GET)
-    public String displayAddCheeseForm(Model model) {
-        model.addAttribute("title", "Add Cheese");
-        model.addAttribute(new Cheese());
-        model.addAttribute("categories", categoryDao.findAll());
-        return "cheese/add";
+    public String add(Model model) {
+
+        model.addAttribute(new Category());
+        model.addAttribute("title", "Add Category");
+
+        return "category/add";
+
     }
 
     @RequestMapping(value = "add", method = RequestMethod.POST)
-    public String processAddCheeseForm(@ModelAttribute  @Valid Cheese newCheese,
-                                       Errors errors,@RequestParam int categoryId, Model model) {
+    public String add(Model model, @ModelAttribute @Valid Category category,
+                      Errors errors) {
 
         if (errors.hasErrors()) {
-            model.addAttribute("title", "Add Cheese");
-            model.addAttribute("categories", categoryDao.findAll());
-            return "cheese/add";
+            model.addAttribute("title", "Categories");
+            return "category/add";
         }
-        Category cat = categoryDao.findOne(categoryId);
-        newCheese.setCategory(cat);
-        cheeseDao.save(newCheese);
+
+        categoryDao.save(category);
         return "redirect:";
+
+
     }
 
-    @RequestMapping(value = "remove", method = RequestMethod.GET)
-    public String displayRemoveCheeseForm(Model model) {
-        model.addAttribute("cheeses", cheeseDao.findAll());
-        model.addAttribute("title", "Remove Cheese");
-        return "cheese/remove";
-    }
 
-    @RequestMapping(value = "remove", method = RequestMethod.POST)
-    public String processRemoveCheeseForm(Model model, @RequestParam int[] cheeseIds) {
 
-        for(int cheeseId : cheeseIds) {
-            cheeseDao.delete(cheeseId);
-        }
-        return "redirect:";
-    }
-
-    @RequestMapping(value = "category", method = RequestMethod.GET)
-    public String categoru(Model model, @RequestParam int id) {
-
-        Category cat = categoryDao.findOne(id);
-        List<Cheese> cheeses = cat.getCheeses();
-        model.addAttribute("cheeses", cheeses);
-        model.addAttribute("title", "cheeses in category: " + cat.getName());
-        return "cheese/index";
-    }
-
-}}
+}
--- src/main/java/org/launchcode/models/data/CategoryDao.java
+++ src/main/java/org/launchcode/models/data/CategoryDao.java
@@ -1,7 +1,7 @@
 package org.launchcode.models.data;
 
 import org.launchcode.models.Category;
-import org.springframework.data.repository.CrudRepository
+import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
 import javax.transaction.Transactional;
